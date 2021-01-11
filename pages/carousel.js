100b81831a69cb5d5dce2f118759089fc2cc0fae
withewolf-dev <itskashyap26@gmail.com>
2021-01-11 15:54:51 +0530

    tailwid

--- pages/carousel.js
+++ pages/carousel.js
@@ -11,11 +11,23 @@ const Carousel = dynamic(
 
 
 const MyCarousel = () => (
-    <Carousel plugins={['arrows']}>
+    <>
+    <div className=" h-60 w-96 flex  items-center lg:hidden">
+        <Carousel plugins={['arrows']}>
       <img src="https://images.unsplash.com/photo-1610297987527-ce5ea2915e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="ads" />
       <img src="https://images.unsplash.com/photo-1610297987527-ce5ea2915e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="asdasd"/>
       <img src="https://images.unsplash.com/photo-1610297987527-ce5ea2915e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="ASDasd"/>
     </Carousel>
+    </div>
+
+    <div className="hidden lg:block">
+        <Carousel plugins={['arrows']}>
+      <img src="https://images.unsplash.com/photo-1610297987527-ce5ea2915e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="ads" />
+      <img src="https://images.unsplash.com/photo-1610297987527-ce5ea2915e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="asdasd"/>
+      <img src="https://images.unsplash.com/photo-1610297987527-ce5ea2915e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="ASDasd"/>
+    </Carousel>
+    </div>
+    </>
   );
   
   export default MyCarousel
